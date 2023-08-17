96de54396ef5532f3605e0ba06355a521e54dad7
carlos lescano <carlos.lescanomorisson@gmail.com>
2023-08-17 18:46:51 -0400

    script de geração de plataforma

---
+++ InfinityRunner/Assets/Scripts/Plataformas/SpawnPlataform.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlataform : MonoBehaviour
+{
+    public List<GameObject> plataforms = new List<GameObject>();
+    private List<Transform> currentPlataforms = new List<Transform>();
+    private Transform player;
+    private Transform currentPlataformPoint;
+    private int plataformIndex;
+    public float offset;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        for (int i=0; i<plataforms.Count; i++)
+        {
+            Transform p =Instantiate(plataforms[i], new Vector2(i*27, 0), transform.rotation).transform;
+            currentPlataforms.Add(p);
+            offset += 27;
+        }
+        currentPlataformPoint = currentPlataforms[plataformIndex].GetComponent<Plataform>().finalPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        float distance = player.position.x - currentPlataformPoint.position.x;
+        if (distance >= 3)
+        {
+            Pooling(currentPlataforms[plataformIndex].gameObject);
+            plataformIndex++;
+            if (plataformIndex > currentPlataforms.Count - 1)
+            {
+                plataformIndex = 0;
+            }
+            currentPlataformPoint = currentPlataforms[plataformIndex].GetComponent<Plataform>().finalPoint;
+        }
+    }
+    public void Pooling(GameObject plataform)
+    {
+        plataform.transform.position = new Vector2(offset,0f);
+        offset += 30f;
+    }
+}
